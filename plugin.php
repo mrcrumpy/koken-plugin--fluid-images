42a6f3c2fc2a329e1db18f0fd11e28f1fc46b8cc
Daniel Crump <crump1991@gmail.com>
2014-07-23 15:50:29 +0200

    Change filter to hook

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@ class Hochkant extends KokenPlugin {
 
     public function __construct() {
         $this->require_setup = true;
-        $this->register_filter('api.content', 'render');
+        $this->register_hook('before_closing_head', 'render');
       }
 
       public function render() {
