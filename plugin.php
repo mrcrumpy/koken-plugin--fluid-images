a0f9e73bfc3ae1fcef5846c13245236622e19442
Daniel Crump <crump1991@gmail.com>
2014-08-03 12:02:26 +0200

    2-4 images beside each other

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 class MHerbstInsertThumbPlugin extends KokenPlugin {
 
 	function __construct() {
-		$this->register_shortcode('mherbst_thumbs', 'render');
+		$this->register_shortcode('hochkant_thumbs', 'render');
 	}
 
 	function render($attributes) 	{
@@ -13,22 +13,21 @@ class MHerbstInsertThumbPlugin extends KokenPlugin {
     	fwrite($handle, "StriPos:".stripos($query, "/text/slug:")."--".stripos($query, "/type:essay/")."\n");
     	fwrite($handle, "Stripos2:".stripos($query, "/type:page/"));
   */  	
+    	if(!empty($attributes['anzahl'])){
+    		$width = 100/$attributes['anzahl'];
+    	}
+    	else{
+    		$width = 50;
+    	}
+    	
+    	$style = 'style="float:left;width:'.$width.'%;"';
 
-		if ((stripos($query, "/text/slug:") === false && !$this->data->show_in_index) && stripos($query, "/type:page/") === false)
-			return "";
-
-		$class = $attributes['class'];
-		if ($class === "") {
-			$class = $this->data->default_css;
-		}
-			
 		return <<<HTML
 		<!-- koken:img {$size} {$preset} {$width} {$height} {$lazy} {$crop} add: {$addStyle} -->
-<div class="k-content {$class}">
+<div class="k-content" {$style}>
 	<koken:load source="content" filter:id="{$attributes['id']}">
 		<figure class="k-content-embed">
-			{$linkbegin}<koken:img  />{$linkend}
-			{$caption}
+			<koken:img  />
 		</figure>
 	</koken:load>
 </div>
